fbf2ab4341b2d6c4939add1bfbb3ea1b5d315ee8
Raphael Kolijn <raphaelkolijn@gmail.com>
2018-03-09 13:19:00 +0100

    Initial commit
    First commit for GTO grid

---
+++ Assets/Map.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour {
+
+    public int SizeX;
+    public int SizeY;
+
+    public float TileOffset;
+
+    public Cell Prototype;
+
+    // public List<Cell> cells;
+
+    public void Start()
+    {
+        Spawn();
+    }
+
+    public void Spawn()
+    {
+        float xOffset = 0;
+        for (int x = 0; x < SizeX; x++)
+        {
+            float yOffset = 0;
+            for (int y = 0; y < SizeY; y++)
+            {
+                Cell newTile = Instantiate(Prototype);
+                newTile.transform.SetParent(transform);
+                newTile.transform.localPosition = new Vector3(xOffset, 0, yOffset);
+                newTile.name = string.Format("Cell {0}x{1}", x, y);
+                // newTile.name = "Cell "+ x + "x" + y;
+                newTile.X = x;
+                newTile.Y = y;
+
+                yOffset += TileOffset;
+            }
+            xOffset += TileOffset;
+        }
+    }
+
+    public Cell getCell(int x, int y)
+    {
+        if(x >= SizeX || y >= SizeY)
+        {
+            Debug.LogError("Out of bounds");
+            return null;
+        }
+        return transform.GetChild((x * SizeY)+ y).GetComponent<Cell>();
+    }
+}
